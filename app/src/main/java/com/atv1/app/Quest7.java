229d1c7ae37ad0d92c3b4f7ec13c85544f988a49
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-21 19:34:12 +0000

    feat(Quest7): create Quest 7 file

---
+++ app/src/main/java/com/atv1/app/Quest7.java
@@ -0,0 +1,46 @@
+package com.atv1.app;
+
+/**
+ * 7.
+ *  Elabore um algoritmo que leia um número inteiro positivo n, 
+ * e preencha duas listas Lx e Ly, cada uma com n valores gerados aleatoriamente 
+ * no intervalo de 1 a 2n. 
+ * 
+ * Em seguida, armazenar em 
+ * uma lista Lz os elementos de Lx que não estão em Ly
+ */
+
+public class Quest7 {
+
+  public static void main(String[] args) {
+    int n = 20;
+
+    ListaEncadeada Lx = new ListaEncadeada();
+    ListaEncadeada Ly = new ListaEncadeada();
+    ListaEncadeada Lz = new ListaEncadeada();
+
+    for (int i = 0; i < n; i++) {
+      int nx = getRandomNumber(1, 2*n);
+      Lx.adicionaFinal(nx);
+      
+      int ny = getRandomNumber(1, 2*n);
+      Ly.adicionaFinal(ny);
+    }
+
+    System.out.println("Lista Lx:");
+    Lx.imprimeLista();
+
+    System.out.println("Lista Ly:");
+    Ly.imprimeLista();
+
+
+    System.out.println("Lista Lz:");
+    Lz.imprimeLista();
+  }
+
+  private static int getRandomNumber(int min, int max) {
+    int number = (int) (Math.random() * (max)) + min;
+    return number;
+  } 
+}
+
